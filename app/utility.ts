dc90dbc3d3dce65490d82052c2fd9d25b445978c
Ramish Hassan <ramishshah93@gmail.com>
2022-06-14 00:29:41 +0200

    bug: wrong variable name in utility class and scoreboard class fixed

--- app/utility.ts
+++ app/utility.ts
@@ -1,6 +1,9 @@
 class Utility {
-static getInputV(id: string): string {
-    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(id)
-        return inputElement.value;
+
+    static getInputValue(elementID: string): string {
+  
+      const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+      return inputElement.value;
     }
-}
+  
+  }
